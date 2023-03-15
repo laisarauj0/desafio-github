e2050183ea42fd2d0b288ad77987790f165a6eab
laisarauj <laisaraujo.g@gmail.com>
2023-03-15 11:58:19 -0300

    Adicionando novos arquivos

---
+++ Sintaxe-Java/anatomia-classe/src/BoletimEstudaltil.java
@@ -0,0 +1,11 @@
+public class BoletimEstudaltil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if(mediaFinal<6)
+        System.out.println("Reprovado");
+        else if(mediaFinal == 6)
+        System.out.println("Prova minerva");
+        else
+        System.out.println("Aprovado");
+    }
+}
